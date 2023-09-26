61a076cebe52a8b3a42fded7693b3fc816a30ec5
wdb <wdb@mz.co.kr>
2023-09-26 15:51:28 +0900

    feat : output - vpc id

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 # network
 module "network" {
   source               = "./terraform-aws-vpc"
-  prefix           = var.prefix
+  prefix               = var.prefix
   vpc_cidr             = var.vpc_cidr
   availability_zones   = var.availability_zones
   public_subnet_cidrs  = var.public_subnet_cidrs
